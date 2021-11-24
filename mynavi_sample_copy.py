c9c91180f8f689243abcaf329b2ac2792aa6b5dd
shou-shou-kun <51499190+shou-shou-kun@users.noreply.github.com>
2021-11-24 16:40:06 +0900

    master

--- mynavi_sample_copy.py
+++ mynavi_sample_copy.py
@@ -11,8 +11,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 # 定数は大文字の変数名が一般的
 # {}に変数名をセットすると、後でformatで置換可能
-LOG_FILE_PATH = "logs/log_{datetime}.log"
-# EXP_CSV_PATH="results/exp_list_{search_keyword}_{datetime}.csv"
+LOG_FILE_PATH = "log_{datetime}.log"
+EXP_CSV_PATH="results/exp_list_{search_keyword}_{datetime}.csv"
 log_file_path=LOG_FILE_PATH.format(datetime=datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
 
 
@@ -95,29 +95,29 @@ def main():
     # 検索ボタンクリック
     driver.find_element_by_class_name("topSearch__button").click()
 
-    def page_view():
+    # def page_view():
     
-        # ページ終了まで繰り返し取得
-        # 検索結果の一番上の会社名を取得
-        name_list = driver.find_elements_by_class_name("cassetteRecruit__name")
-        
-        # 空のDataFrame作成
-        df = pd.DataFrame()
+    # ページ終了まで繰り返し取得
+    # 検索結果の一番上の会社名を取得
+    name_list = driver.find_elements_by_class_name("cassetteRecruit__name")
+    
+    # 空のDataFrame作成
+    df = pd.DataFrame()
 
-        # 1ページ分繰り返し
-        print(len(name_list))
-        for name in name_list:
-            print(name.text)
-            # DataFrameに対して辞書形式でデータを追加する
-            df = df.append(
-                {"会社名": name.text, 
-                "項目B": "",
-                "項目C": ""}, 
-                ignore_index=True)
-            
-        # csv出力
-        df.to_csv('to_csv_out.csv', mode = 'a', header = False) 
-           
+    # 1ページ分繰り返し
+    print(len(name_list))
+    for name in name_list:
+        print(name.text)
+        # DataFrameに対して辞書形式でデータを追加する
+        df = df.append(
+            {"会社名": name.text, 
+            "項目B": "",
+            "項目C": ""}, 
+            ignore_index=True)
+        
+    # csv出力
+    df.to_csv('to_csv_out.csv', mode = 'a', header = False)
+
     # ２ページ目（以降）の表示
     # ページ終了まで繰り返し取得
     while True:
@@ -131,28 +131,28 @@ def main():
         
         
         
-        page_view()
-        # # 検索結果の一番上の会社名を取得
-        # name_list = driver.find_elements_by_class_name("cassetteRecruit__name")
+        # page_view()
+        # 検索結果の一番上の会社名を取得
+        name_list = driver.find_elements_by_class_name("cassetteRecruit__name")
         
-        # # 空のDataFrame作成
-        # df = pd.DataFrame()
+        # 空のDataFrame作成
+        df = pd.DataFrame()
 
         
         
-        # # 1ページ分繰り返し
-        # print(len(name_list))
-        # for name in name_list:
-        #     print(name.text)
-        #     # DataFrameに対して辞書形式でデータを追加する
-        #     df = df.append(
-        #         {"会社名": name.text, 
-        #         "項目B": "",
-        #         "項目C": ""}, 
-        #         ignore_index=True)
+        # 1ページ分繰り返し
+        print(len(name_list))
+        for name in name_list:
+            print(name.text)
+            # DataFrameに対して辞書形式でデータを追加する
+            df = df.append(
+                {"会社名": name.text, 
+                "項目B": "",
+                "項目C": ""}, 
+                ignore_index=True)
             
-        # # csv出力
-        # df.to_csv('to_csv_out.csv', mode = 'a', header = False)
+        # csv出力
+        df.to_csv('to_csv_out.csv', mode = 'a', header = False)
        
 
 # 直接起動された場合はmain()を起動(モジュールとして呼び出された場合は起動しないようにするため)
